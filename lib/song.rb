a5b6137be975eeef4bb512d90331a8b44e5e131a
Tracy Ngo <tngo1206@gmail.com>
2019-03-25 00:44:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
 require 'pry'
 
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
-  include Memorable::InstanceMethods
+  
   
   attr_accessor :name
   attr_reader :artist
