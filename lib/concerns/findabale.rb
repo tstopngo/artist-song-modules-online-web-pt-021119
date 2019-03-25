520fb58cc1adec7a201863200fe34904657d01ef
Tracy Ngo <tngo1206@gmail.com>
2019-03-25 00:28:07 +0000

    Automatically backed up by Learn

--- lib/concerns/findabale.rb
+++ lib/concerns/findabale.rb
@@ -1,10 +1,7 @@
 module Findable
 
   def find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    all.detect{|a| a.name == name}
   end
   
-   def find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 end
